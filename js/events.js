cb150aaaf7a71ab55b7c1c691ad1830969e51bbc
petardevs <124912810+petardevs@users.noreply.github.com>
2026-01-23 19:15:14 +0100

    new events

--- js/events.js
+++ js/events.js
@@ -5,8 +5,86 @@ const events = [
       napomena: "Колоквијум - задаци"
    },
    {
-      naziv: "Јануарски рок",
-      datum: "18.2",
-      napomena: "Испити"
+      naziv: "АРОС",
+      datum: "22.2",
+      napomena: "Усмени"
+   },
+   {
+      naziv: "АРОС",
+      datum: "24.2",
+      napomena: "Писмени"
+   },
+   {
+      naziv: "ДМС",
+      datum: "20.2",
+      napomena: "Писмени"
+   },
+   {
+      naziv: "ДМС",
+      datum: "26.2",
+      napomena: "Усмени"
+   },
+   {
+      naziv: "Програмирање",
+      datum: "21.2",
+      napomena: "Испит"
+   },
+   {
+      naziv: "Теорија вероватноће",
+      datum: "23.2",
+      napomena: "Писмени"
+   },
+   {
+      naziv: "Теорија вероватноће",
+      datum: "28.2",
+      napomena: "Усмени"
+   },
+   {
+      naziv: "Управљање пројектима",
+      datum: "25.2",
+      napomena: "Други део испита"
+   },
+
+
+
+   {
+      naziv: "АРОС",
+      datum: "7.3",
+      napomena: "Усмени"
+   },
+   {
+      naziv: "АРОС",
+      datum: "9.3",
+      napomena: "Писмени"
+   },
+   {
+      naziv: "ДМС",
+      datum: "5.3",
+      napomena: "Писмени"
+   },
+   {
+      naziv: "ДМС",
+      datum: "11.3",
+      napomena: "Усмени"
+   },
+   {
+      naziv: "Програмирање",
+      datum: "6.3",
+      napomena: "Испит"
+   },
+   {
+      naziv: "Теорија вероватноће",
+      datum: "8.3",
+      napomena: "Писмени"
+   },
+   {
+      naziv: "Теорија вероватноће",
+      datum: "13.3",
+      napomena: "Усмени"
+   },
+   {
+      naziv: "Управљање пројектима",
+      datum: "10.3",
+      napomena: "Други део испита"
    },
 ];
